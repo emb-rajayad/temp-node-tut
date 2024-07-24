ed4e60d21e432c3af701da080371544def35f96e
emb-rajayad <rajan.yadav@embitel.com>
2024-07-24 10:37:57 +0530

    update lodash folder and code

--- app.js
+++ app.js
@@ -37,7 +37,7 @@
 // console.log(names)
 
 
-const _ = require('loadash');
+const _ = require('lodash');
 const items = [1, [2, [3, [4]]]]
 const newItems = _.flattenDeep(items)
 console.log(newItems)
